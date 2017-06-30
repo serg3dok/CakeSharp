206948e091e8e8478538d0692993f48b0075d90a
Sergey Okinchuk <serg3d@hotmail.com>
2017-06-30 16:37:55 -0700

    Solving FindRotationPoint using binary search

--- FindRotationPoint/Program.cs
+++ FindRotationPoint/Program.cs
@@ -8,8 +8,51 @@ namespace FindRotationPoint
 {
     class Program
     {
+
+        /*
+         * Write a function for finding the index of the "rotation point," 
+         * which is where I started working from the beginning of the dictionary. 
+         * This array is huge (there are lots of words I don't know) so we want to be efficient here. 
+         */
+
         static void Main(string[] args)
         {
+            char[] charArr = {'k', 'v', 'a', 'b', 'c', 'd', 'e', 'g', 'i'};
+
+            Console.WriteLine(rotationPointAtIndex(charArr));
+            Console.Read();
         }
+
+        public static int rotationPointAtIndex(char[] charArr)
+        {
+            int startIndex = -1;
+            int endIndex = charArr.Length-1;
+            int index = 0;
+
+            while (charArr[startIndex+1] > charArr[endIndex])
+            {
+
+                int distance = endIndex - startIndex;
+                int halfDistance = distance / 2;
+                int guessIndex = startIndex + halfDistance;
+                char guessValue = charArr[guessIndex];
+
+                if (charArr[startIndex] > guessValue)
+                {
+                    endIndex = guessIndex;
+                }
+                if (charArr[startIndex] < guessValue)
+                {
+                    startIndex = guessIndex;
+                }
+                index = guessIndex;
+            }
+
+            return index;
+
+        }
+
     }
+
+
 }
