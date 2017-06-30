5761b82c77ecd18ce1e52fe965f017a1556cc54f
Sergey Okinchuk <serg3d@hotmail.com>
2017-06-29 17:19:29 -0700

    Problem 10 solved

---
+++ SecondLargestNumberInBST/BinaryTree.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SecondLargestNumberInBST
+{
+    class BinaryTree
+    {
+        public int Value;
+        public BinaryTree Left;
+        public BinaryTree Right;
+
+
+        public BinaryTree(int value)
+        {
+            Value = value;
+        }
+
+        public static BinaryTree CreateBinaryTree()
+        {
+            BinaryTree tree = new BinaryTree(50);
+
+            // left side
+            tree.Left = new BinaryTree(25);
+            tree.Left.Left = new BinaryTree(10);
+            tree.Left.Right = new BinaryTree(35);
+            tree.Left.Right.Right = new BinaryTree(40);
+
+            // right side
+            tree.Right = new BinaryTree(75);
+            tree.Right.Left = new BinaryTree(60);
+            tree.Right.Right = new BinaryTree(85);
+            tree.Right.Right.Right = new BinaryTree(100);
+            tree.Right.Right.Right.Left = new BinaryTree(90);
+            tree.Right.Right.Right.Right = new BinaryTree(110);
+
+
+
+            return tree;
+        }
+
+    }
+}
--- SecondLargestNumberInBST/Program.cs
+++ SecondLargestNumberInBST/Program.cs
@@ -8,8 +8,48 @@ namespace SecondLargestNumberInBST
 {
     class Program
     {
+
+        /*
+         * Write a function to find the 2nd largest element in a binary search tree
+         * */
+
         static void Main(string[] args)
         {
+            
+
+            BinaryTree tree = BinaryTree.CreateBinaryTree();
+
+
+            Console.WriteLine(secondNumber(tree));
+            Console.Read();
+            
         }
+
+        public static int secondNumber(BinaryTree tree)
+        {
+
+            // second largest element should be located on right branch before last one
+            // so we can go down on right branches and keep in mind previous value
+            // if no right branch from root, so return value from root.left
+
+            if (tree.Right == null)
+            {
+                if (tree.Left != null)
+                {
+                    return tree.Left.Value;
+                }
+                throw new ArgumentException();
+            }
+
+            int result = tree.Value;
+            while (tree.Right != null)
+            {
+                result = tree.Value;
+                tree = tree.Right;
+            }
+            return result;
+        }
+
+
     }
 }
