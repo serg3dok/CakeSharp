dfaceda4662eb3248fee77bcd6c28af715311939
Sergey Okinchuk <serg3d@hotmail.com>
2017-06-30 11:15:45 -0700

    MillionGazilion solved

--- MillionGazillion/Program.cs
+++ MillionGazillion/Program.cs
@@ -10,6 +10,71 @@ namespace MillionGazillion
     {
         static void Main(string[] args)
         {
+            Trie trie = new Trie();
+
+            string[] urls =
+            {
+                "google.com",
+                "google.com/maps",
+                "google.com/maps/gps",
+                "google.com/about",
+                "google.com/about/contacts",
+                "google.com/about/job"
+            };
+
+            string word = "sometext";
+
+            trie = addToTrie(trie, word);
+
+            bool isWordInTrie = checkWordInTrie(trie, word);
+            Console.WriteLine(isWordInTrie);
+            Console.Read();
+
+        }
+
+        private static Trie addToTrie(Trie trie, string word)
+        {
+            char endOfWord = '\0';
+            Trie currentNode = trie;
+            foreach (var c in word)
+            {
+                if (!currentNode.hasChildNode(c))
+                {
+                    currentNode.makeNewNode(c);
+                }
+                currentNode = currentNode.getTrieNode(c);
+            }
+            if (!currentNode.hasChildNode(endOfWord))
+            {
+                currentNode.makeNewNode(endOfWord);
+            }
+
+            return trie;
+        }
+
+        private static bool checkWordInTrie(Trie trie, string word)
+        {
+            char endOfWord = '\0';
+            Trie currentNode = trie;
+            bool result = false;
+            foreach (var c in word)
+            {
+                if (currentNode.hasChildNode(c))
+                {
+                    currentNode = currentNode.getTrieNode(c);
+                }
+                else
+                {
+                    return result;
+                }
+                
+            }
+            if (currentNode.hasChildNode(endOfWord))
+            {
+                result = true;
+            }
+
+            return result;
         }
     }
 }
---
+++ MillionGazillion/Trie.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+
+namespace MillionGazillion
+{
+    public class Trie
+    {
+        Dictionary<char, Trie> TrieNode = new Dictionary<char, Trie>();
+
+        public bool hasChildNode(char c)
+        {
+            return TrieNode.ContainsKey(c);
+        }
+
+        public void makeNewNode(char c)
+        {
+            TrieNode[c] = new Trie();
+        }
+
+        public Trie getTrieNode(char c)
+        {
+            return TrieNode[c];
+        }
+
+
+    }
+}
