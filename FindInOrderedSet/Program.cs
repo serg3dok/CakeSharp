b2595b4f3be28d2d29759ce6b2323d31d23e2616
Sergey Okinchuk <serg3d@hotmail.com>
2017-06-30 13:34:52 -0700

    Added Problem 12. Find in ordered set

---
+++ FindInOrderedSet/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindInOrderedSet
+{
+    class Program
+    { 
+        /*
+         * Suppose we had an array of nnn integers sorted in ascending order. 
+         * How quickly could we check if a given integer is in the array?
+         */
+
+        static void Main(string[] args)
+        {
+            // we can use binary search aproach
+            // divide by 2 recurivelly until find given number
+
+            
+        }
+    }
+}
