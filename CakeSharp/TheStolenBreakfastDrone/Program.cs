4d645928595a4c4e1cdbf8dbdb2015e3e7d4599f
Sergey Okinchuk <serg3d@hotmail.com>
2017-07-06 10:26:36 -0700

    21 problem solved using sorting

--- CakeSharp/TheStolenBreakfastDrone/Program.cs
+++ CakeSharp/TheStolenBreakfastDrone/Program.cs
@@ -17,6 +17,29 @@ namespace TheStolenBreakfastDrone
     {
         static void Main(string[] args)
         {
+            int[] id = {123123500, 123123500, 091165890, 091165890,
+                197987500, 197987500, 008456789, 170952377,
+                170952377, 097111588, 097111588, 097111588,
+                789225897, 789225897, 422246625, 422246625};
+            Console.WriteLine(findUnicNumber(id));
+            Console.Read();
+
         }
+
+        public static int findUnicNumber(int[] idArr)
+        {
+            Array.Sort(idArr);
+
+            for (int i = 0; i < idArr.Length - 1; i++)
+            {
+                if (idArr[i] != idArr[i + 1])
+                {
+                    return idArr[i];
+                }
+            }
+            return idArr[idArr.Length - 1];
+
+        }
+        // O(nlogn)/O(1)
     }
 }
