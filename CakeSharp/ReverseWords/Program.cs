30989af4e497ad6975e5914eebe6504b0d870963
Sergey Okinchuk <serg3d@hotmail.com>
2017-07-07 11:25:06 -0700

    27 - reverse words solved

--- CakeSharp/ReverseWords/Program.cs
+++ CakeSharp/ReverseWords/Program.cs
@@ -18,15 +18,59 @@ namespace ReverseWords
         static void Main(string[] args)
         {
             string message = "message coded test a is This";
+            //                ^                          ^
+            //                sihT si a tset
+            //                   ^
+
+            Console.WriteLine(ReverseWords(message));
+            Console.ReadKey();
 
         }
 
         public static string ReverseWords(string str)
         {
             char[] charArr = str.ToCharArray();
+            int startIndex = 0;
+            int endIndex = charArr.Length - 1;
 
+            while (startIndex < endIndex)
+            {
+                char tmp = charArr[endIndex];
+                charArr[endIndex] = charArr[startIndex];
+                charArr[startIndex] = tmp;
+                startIndex++;
+                endIndex--;
+            } 
+            
+            for (int i = 0; i < charArr.Length; i++)
+            {
+                startIndex = i;
+                endIndex = i;
+                while (charArr[endIndex] != ' ')
+                {
+                    if (endIndex + 1 == charArr.Length)
+                    {
+                        break;
+                    }
+                    endIndex++;
+                    i = endIndex;
+                }
 
+                if (charArr[endIndex] == ' ')
+                {
+                    endIndex--;
+                }
 
+                while (startIndex < endIndex)
+                {
+                    char tmp = charArr[endIndex];
+                    charArr[endIndex] = charArr[startIndex];
+                    charArr[startIndex] = tmp;
+                    startIndex++;
+                    endIndex--;
+                }
+            }
+            
             return new string(charArr);
         }
     }
