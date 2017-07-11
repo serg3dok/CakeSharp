0b33644f314d8b697fd62393e7085f19059ac0e0
Sergey Okinchuk <serg3d@hotmail.com>
2017-07-10 18:52:25 -0700

    33 Which number appears twice

---
+++ CakeSharp/WhichAppearsTwice/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WhichAppearsTwice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = {0, 2, 3, 15, 25, 64, 7, 88, 1, 3, 16, 99};
+            Console.WriteLine(findTwice(numbers));
+            Console.Read();
+        }
+
+        public static int findTwice(int[] nums)
+        {
+            var map = new Dictionary<int, bool>();
+            for (int i = 0; i < nums.Length; i++)
+            {
+                bool b;
+                map.TryGetValue(nums[i], out b);
+                if (b)
+                {
+                    return nums[i];
+                } 
+                map.Add(nums[i], true);
+            }
+
+            return -1;
+        }
+    }
+}
+
+
